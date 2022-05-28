c7cffa64e9dab5159cc1f40a77f01c846505a1eb
koyu <me@koyu.space>
2022-05-28 12:28:49 +0200

    Backup ejabberd

--- runmaintenance.sh
+++ runmaintenance.sh
@@ -3,7 +3,7 @@ systemctl stop mastodon-* --all
 su - postgres -c pg_dumpall > /home/koyu/koyuspace.db
 systemctl start mastodon-* --all
 export RESTIC_PASSWORD=hellno
-restic -o rclone.args="serve restic --stdio --verbose" -r rclone:onedrive:koyuspace_backups backup /home/mastodon /etc/nginx /var/lib/redis /home/gemini/ /home/koyu/ /usr/bin/runmaintenance /var/lib/tor/ /etc/tor /etc/letsencrypt --verbose
+restic -o rclone.args="serve restic --stdio --verbose" -r rclone:onedrive:koyuspace_backups backup /home/mastodon /etc/nginx /var/lib/redis /home/gemini/ /home/koyu/ /usr/bin/runmaintenance /var/lib/tor/ /etc/tor /etc/letsencrypt /opt/ejabberd-22.05 /opt/ejabberd --verbose
 rm /home/koyu/koyuspace.db
 sudo -i -u mastodon bash << EOF
 cd /home/mastodon/live/
