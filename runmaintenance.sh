bd45a6b37b320e7ff1dadc3bd41637cba52e041d
koyu <me@koyu.space>
2022-10-07 03:27:13 +0200

    Update runmaintenance.sh

--- runmaintenance.sh
+++ runmaintenance.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-systemctl stop mastodon-* --all
-su - postgres -c pg_dumpall > /home/koyu/koyuspace.db
-systemctl start mastodon-* --all
 export RESTIC_PASSWORD=hellno
 restic -o rclone.args="serve restic --stdio --verbose" -r rclone:onedrive:koyuspace_backups backup /home/mastodon /etc/nginx /var/lib/redis /home/gemini/ /home/koyu/ /usr/bin/runmaintenance /var/lib/tor/ /etc/tor /etc/letsencrypt /opt/ejabberd-22.05 /opt/ejabberd /var/www --verbose
-rm /home/koyu/koyuspace.db
-sudo -i -u mastodon bash << EOF
-cd /home/mastodon/live/
-tootctl media remove --concurrency 50
-EOF
