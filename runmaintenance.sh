8f2c5b1adae802c0f3fea201b5ddbc3d2dec06a1
koyu <me@koyu.space>
2023-01-29 01:27:05 +0100

    Update runmaintenance.sh

--- runmaintenance.sh
+++ runmaintenance.sh
@@ -3,3 +3,4 @@ sudo systemctl stop mastodon-* --all
 sudo -u postgres pg_dumpall > /root/db.out
 export RESTIC_PASSWORD=hellno
 restic -o rclone.args="serve restic --stdio --verbose" -r rclone:onedrive:koyuspace_backups backup /home/mastodon /etc/nginx /var/lib/redis /root/db.out /home/koyu
+sudo systemctl start mastodon-* --all
